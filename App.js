551c3f3b9ab511d52b322d438a8a8470530e0072
fpastl <75983960+fpastl@users.noreply.github.com>
2021-04-10 16:47:45 +0300

    init:react-native project

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello Word!</Text>
+      </View>
+    );
+  }
+}
+
+// const styles = StyleSheet.create({
+// });
+
+export default App;
