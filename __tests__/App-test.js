f4bd8db00cc4aeb04af1c24993922cc34505a29b
Anton Skarabahaty <antoniosk10@gmail.com>
2022-04-08 16:39:30 +0500

    feat: add snapshot test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,28 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import ModalWindow from '../src/components/ModalWindow';
+
+import {Provider} from 'react-redux';
+import {render} from '@testing-library/react-native';
+import store from '../src/redux/store/store';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('renders App correctly', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+describe('Test redux', () => {
+  test('render ModalWindow', () => {
+    const component = (
+      <Provider store={store}>
+        <ModalWindow fields={['avatar', 'first_name', 'last_name', 'email']} />
+      </Provider>
+    );
+    const tree = render(component).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
